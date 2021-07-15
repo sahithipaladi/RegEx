a8fe9fe83120bca9993dd4fec0f46ff329d25adb
sahithi <sahithipari99@gmail.com>
2021-07-15 12:12:01 +0530

    [sahithi] Adding password

--- UserRegistration/User.cs
+++ UserRegistration/User.cs
@@ -72,7 +72,7 @@ namespace UserRegistration
                 Console.WriteLine("It is not valid");
         }
         //Pattern for validating the password
-        string passwordPattern = "^[A-Za-z1-9.@!#$&]{8,}$";
+        string passwordPattern = "^(?=.*[A-Z])[A-Za-z1-9.@!#$&%^*]{8,}$";
         /// <summary>
         /// UC - 5,6  Validating the Password
         /// </summary>
