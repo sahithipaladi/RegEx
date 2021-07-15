c3abc49fe94d4e301fa864c5ec9ed1630c924e50
sahithi <sahithipari99@gmail.com>
2021-07-15 11:47:31 +0530

    [sahithi] Intial commit

---
+++ RegExpression/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("--------------User Registration----------------");
+                User user = new User();
+                user.ValidateFirstName();
+
+            }
+        }
+    }
+
+    
+
---
+++ RegExpression/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace RegExpression
+{
+    class User
+    {
+        string pattern = "^[A-Z]{1}[A-Za-z]{2,}$";
+        /// <summary>
+        /// UC - 1  Validating the First Name
+        /// </summary>
+        public void ValidateFirstName()
+        {
+            Console.WriteLine("Enter a First Name to validate : ");
+            string firstName = Console.ReadLine();
+            Regex regex = new Regex(pattern);
+            bool result = regex.IsMatch(firstName);
+            if (result)
+                Console.WriteLine("It is valid");
+            else
+                Console.WriteLine("It is not valid");
+        }
+    }
+}
+    
+
