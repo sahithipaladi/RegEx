1d36c5a83f51f56460923ee78722c77d8fd21701
sahithipaladi <86294777+sahithipaladi@users.noreply.github.com>
2021-07-15 12:26:20 +0530

    Update Program.cs

--- RegExpression/Program.cs
+++ RegExpression/Program.cs
@@ -1,23 +1,79 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace RegExpression
+namespace UserRegistration
 {
     class Program
     {
         static void Main(string[] args)
         {
-                Console.WriteLine("--------------User Registration----------------");
+            Console.WriteLine("--------------User Registration----------------");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.First Name Validation");
+                Console.WriteLine("2.Last Name Validation");
+                Console.WriteLine("3.Email Validation");
+                Console.WriteLine("4.Mobile Number Validation");
+                Console.WriteLine("5.Password Validation");
+                Console.WriteLine("6.Validating Sample emails");
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("Choose an option : ");
+                int option = Convert.ToInt32(Console.ReadLine());
                 User user = new User();
-                user.ValidateFirstName();
-
-            }
+                switch (option)
+                {
+                    case 1:
+                        user.ValidateFirstName();
+                        break;
+                    case 2:
+                        user.ValidateLastName();
+                        break;
+                    case 3:
+                        user.ValidateEmail();
+                        break;
+                    case 4:
+                        user.ValidateMobileNumber();
+                        break;
+                    case 5:
+                        user.ValidatePassword();
+                        break;
+                    case 6:
+                        // Validating Sample Emails............
+                        List<string> sampleEmailList = new List<string>()
+                        {
+                            "abc@yahoo.com",
+                            "abc-100@yahoo.com",
+                            "abc.100@yahoo.com",
+                            "abc111@abc.com",
+                            "abc-100@abc.net",
+                            "abc.100@abc.com.au",
+                            "abc@1.com",
+                            "abc@gmail.com.com",
+                            "abc+100@gmail.com"
+                        };
+                        foreach (string emails in sampleEmailList)
+                        {
+                            if (user.ValidateSampleEmail(emails))
+                            { 
+                                Console.WriteLine(" [ " + emails + " ]\t is Valid Email");
+                            }
+                            else
+                            {
+                                Console.WriteLine(" [ " + emails + " ]\t is Invalid Email");
+                            }
+                        }
+                        break;
+                    case 0:
+                        flag = false;
+                        break;
+                    default:
+                        break;
+                }
+            }   
         }
     }
+}
 
     
 
