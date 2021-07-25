8a4e7d743f2c52fd19dc1a851c26a1a95bec6312
sahithi <sahithipari99@gmail.com>
2021-07-25 20:11:44 +0530

    [sahithi] Lamda Function

--- UserRegistration/User.cs
+++ UserRegistration/User.cs
@@ -11,56 +11,19 @@ namespace UserRegistration
     {
         //Pattern for validating the name
         string namePattern = "^[A-Z]{1}[A-Za-z]{2,}$";
-        /// <summary>
-        /// UC - 1  Validating the First Name
-        /// </summary>
-        public bool ValidateFirstName(string firstName)
-        {
-            Regex regex = new Regex(namePattern);
-            bool result = regex.IsMatch(firstName);
-            return result;
-        }
-        /// <summary>
-        /// UC - 2  Validating the Last Name
-        /// </summary>
-        public bool ValidateLastName(string lastName)
-        {
-            Regex regex = new Regex(namePattern);
-            bool result = regex.IsMatch(lastName);
-            return result;
-        }
+        public bool ValidateFirstName(string firstName) => (Regex.IsMatch(firstName, namePattern));
+        public bool ValidateLastName(string lastName) => (Regex.IsMatch(lastName, namePattern));
+
         //Pattern for validating the email
         string emailPattern = "^[A-Za-z0-9]+([._+-][A-Za-z0-9]+)*@[0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
-        /// <summary>
-        /// UC - 3,9  Validating the Email
-        /// </summary>
-        public bool ValidateEmail(string email)
-        {
-            Regex regex = new Regex(emailPattern);
-            bool result = regex.IsMatch(email);
-            return result;
-        }
+        public bool ValidateEmail(string email) => (Regex.IsMatch(email, emailPattern));
+
         //Pattern for validating the Mobile Number
         string numberPattern = "^[1-9]{1}[0-9]{0,3}\\s[1-9]{1}[0-9]{9}$";
-        /// <summary>
-        /// UC - 4  Validating the Mobile Number
-        /// </summary>
-        public bool ValidateMobileNumber(string mobileNumber)
-        {
-            Regex regex = new Regex(numberPattern);
-            bool result = regex.IsMatch(mobileNumber);
-            return result;
-        }
+        public bool ValidateMobileNumber(string mobileNumber) => (Regex.IsMatch(mobileNumber, numberPattern));
+
         //Pattern for validating the password
         string passwordPattern = "^(?=.*[A-Z])(?=.*[0-9])(?=[\\w]*[\\W][\\w]*$)[A-Za-z1-9.@!#$&%^*]{8,}";
-        /// <summary>
-        /// UC - 5,6,7,8  Validating the Password
-        /// </summary>
-        public bool ValidatePassword(string password)
-        {
-            Regex regex = new Regex(passwordPattern);
-            bool result = regex.IsMatch(password);
-            return result;
-        }
+        public bool ValidatePassword(string password) => (Regex.IsMatch(password, passwordPattern));
     }
 }
