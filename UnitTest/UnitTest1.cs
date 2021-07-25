43587def209ba3d5bf4c96e06cb91a6429b43145
sahithi <sahithipari99@gmail.com>
2021-07-25 19:59:05 +0530

    [sahithi] Doing Unit parameterized testing

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -118,5 +118,19 @@ namespace UnitTest
             //Assert
             Assert.IsTrue(result);
         }
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void ValidateSampleEmailsShouldReturnTrue(string email)
+        {
+            Assert.IsTrue(user.ValidateEmail(email));
+        }
     }
 }
